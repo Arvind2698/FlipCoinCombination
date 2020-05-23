ae11bf9f109a82df1b49079e8e90cbe76e7c4821
Arvind2698 <r.arvind007@gmail.com>
2020-05-24 00:43:28 +0530

    UC2 Singlet toss combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,13 +1,39 @@
-#!/bin/bash
+#!/bin/bash 
 
 echo Welcon to the flip coin combination program
 
-flip=$((RANDOM%2))
+read -p "How many time do you want to flip the coin" iteration
 
-if [[ $flip -eq 1 ]]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+headWinCounter=0
+tailWinCounter=0
 
+declare -A result
+
+for((count=0;count<$iteration;count++))
+do
+	flip=$((RANDOM%2))
+	if [[ $flip -eq 1 ]]
+	then
+		if [[ ${result["H"]} -eq null ]]
+		then
+			result["H"]=1
+		else
+			result["H"]=$((${result["H"]}+1))
+		fi
+
+	else
+	 	if [[ ${result["T"]} -eq null ]]
+      then
+         result["T"]=1
+      else
+         result["T"]=$((${result["T"]}+1))
+      fi
+
+	fi
+done
+
+
+for i in ${!result[@]}
+do
+	echo "$i => ${result[$i]}"
+done
