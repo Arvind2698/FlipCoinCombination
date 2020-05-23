3cd548879e58b9e51022ce2fee54190e4f11116f
Arvind2698 <r.arvind007@gmail.com>
2020-05-24 01:03:13 +0530

    UC3 Doublet combination of two simultanious coin toss

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -2,12 +2,65 @@
 
 echo Welcon to the flip coin combination program
 
-flip=$((RANDOM%2))
 
-if [[ $flip -eq 1 ]]
-then
-	echo HEAD
-else
-	echo TAIL
-fi
+declare -A result
 
+read -p "How many time do you want to toss" iteration
+
+
+for ((count=0;count<$iteration;count++))
+do
+
+	flip1=$((RANDOM%2))
+	flip2=$((RANDOM%2))
+
+	if [[ $flip1 -eq 1 ]]
+	then
+
+		if [[ $flip2 -eq 1 ]]
+		then
+			if [[ ${result["HH"]} -eq null ]]
+			then
+				result["HH"]=1
+			else
+				result["HH"]=$((${result["HH"]}+1))
+			fi
+		else
+			if [[ ${result["HT"]} -eq null ]]
+         then
+            result["HT"]=1
+         else
+            result["HT"]=$((${result["HT"]}+1))
+         fi
+		fi
+
+	else
+
+		if [[ $flip2 -eq 1 ]]
+      then
+         if [[ ${result["TH"]} -eq null ]]
+         then
+            result["TH"]=1
+         else
+            result["TH"]=$((${result["TH"]}+1))
+         fi
+      else
+         if [[ ${result["TT"]} -eq null ]]
+         then
+            result["TT"]=1
+         else
+            result["TT"]=$((${result["TT"]}+1))
+      	fi
+
+		fi
+
+	fi
+
+
+done
+
+
+for i in ${!result[@]}
+do
+	echo "$i => ${result[$i]}"
+done
